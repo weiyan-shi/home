ecfd6a186ee9154682573993ffbad0d0f4b1dc6d
weiyan.shi <weiyan.shi@momenta.ai>
2024-04-17 19:52:57 +0800

    feat: build structure

---
+++ src/config.js
@@ -0,0 +1,26 @@
+export const menuItems = [
+  {
+    label: 'About',
+    key: 'About',
+  },
+  {
+    label: 'CV',
+    key: 'CV',
+  },
+  {
+    label: 'Publications',
+    key: 'Publications',
+  },
+  {
+    label: 'Teaching',
+    key: 'Teaching',
+  },
+  {
+    label: 'Service',
+    key: 'Service',
+  },
+  {
+    label: 'Experience',
+    key: 'Experience',
+  }
+];
---
+++ src/home.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { Layout, Menu, theme } from 'antd';
+import {menuItems} from './config';
+import Bio from './components/bio'
+import './home.css'
+const { Header, Content, Footer, Sider } = Layout;
+
+
+const Home = () => {
+  const [current, setCurrent] = useState('mail');
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <div className='home'>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Header
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <div className="demo-logo" />
+          <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={menuItems} theme="dark"
+            defaultSelectedKeys={['2']}
+            style={{
+              flex: 1,
+              minWidth: 0,
+            }} />
+        </Header>
+        <Content
+          style={{
+            padding: '0 48px',
+          }}
+        >
+          <Layout
+            style={{
+              padding: '24px 0',
+              margin: '40px 0',
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Sider
+              style={{
+                background: colorBgContainer,
+              }}
+              width={200}
+            >
+              <Bio />
+            </Sider>
+            <Content
+              style={{
+                padding: '0 24px',
+                minHeight: 280,
+              }}
+            >
+              Content
+            </Content>
+          </Layout>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </div>
+  )
+}
+
+export default Home;
